4b3a76ff1327f5d7aa55e93358c33a044499c40f
Hanqi Zhang <hanqizhang@Hanqis-MBP.attlocal.net>
2020-10-07 16:38:44 -0400

    home page

---
+++ screens/CameraScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View} from 'react-native';
+
+function CameraScreen({navigation}) {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Camera Screen</Text>
+      </View>
+    );
+}
+
+export default CameraScreen;
---
+++ screens/NewsScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View} from 'react-native';
+
+function NewsScreen({navigation}) {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>News</Text>
+      </View>
+    );
+}
+
+export default NewsScreen;
