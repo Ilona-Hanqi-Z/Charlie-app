133d293ff77cd1a77fda532ac5c274cc2aae7467
Hanqi Zhang <hanqizhang@Hanqis-MBP.attlocal.net>
2020-10-22 20:57:16 -0400

    assignment testing

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -3,19 +3,53 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import MapView from 'react-native-maps';
+import * as Permissions from 'expo-permissions';
+import * as Location from 'expo-location';
+import { LocationAccuracy } from 'expo-location';
 
 function MapScreen() {
-    return (
+    const [region, setRegion] = React.useState({
+        latitude: "",
+        longitude: "",
+        latitudeDelta: "",
+        longitudeDelta: "",
+    });
+
+    React.useEffect(() => {
+      const getLocationAsync = async() => {
+        let { status } = await Permissions.askAsync(Permissions.LOCATION);
+        if(status !== 'granted'){
+          console.log('Permission to access location was denied.')
+        }
+  
+        let location = await Location.getCurrentPositionAsync({accuracy:LocationAccuracy.Highest});
+        console.log(location);
+        let region = {
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+          latitudeDelta: 0.045,
+          longitudeDelta: 0.045
+        }
+  
+        this.setRegion({region: region});
+      }
+  
+      getLocationAsync();
+      
+    }, );
+
+    
+
+
+    return (  
       
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <MapView
           style={styles.map}
-          initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
+          initialRegion={region}
+          showsUserLocation={true}
+          showsCompass={true}
+          rotateEnabled={false}
         />
       </View>
     );
