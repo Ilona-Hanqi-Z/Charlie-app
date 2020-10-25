d5f80b04b6406b949e5e576e71c8841cd731075b
Hanqi Zhang <hanqizhang@Hanqis-MBP.attlocal.net>
2020-10-25 15:37:14 -0400

    camera

--- model/assignment.js
+++ model/assignment.js
@@ -7,23 +7,41 @@
         title: "test 1",
         caption: 'caption1', 
         address: '203 Conner Drive, Chapel Hill, NC 27514', 
-        latitude:35.930950,
-        longitude:-79.031500
     },
     {
         id: 2, 
         title: "test 2",
         caption: 'caption2', 
         address: 'E 74th St, New York, NY 10021',
-        latitude:40.769119,
-        longitude:-73.955040
     },
     {
         id: 3, 
         title: "test 3",
         caption: 'caption3', 
         address: '400 Broad St, Seattle, WA 98109',
-        latitude:47.620422,
-        longitude:-122.349358
+    },
+    {
+        id: 4, 
+        title: "test 4",
+        caption: 'caption4', 
+        address: '1 10th St, San Francisco, CA 94103',
+    },
+    {
+        id: 5, 
+        title: "test 5",
+        caption: 'caption5', 
+        address: '1100 California St, San Francisco, CA 94108',
+    },
+    {
+        id: 6, 
+        title: "test 6",
+        caption: 'caption6', 
+        address: '1042 Larkin St, San Francisco, CA 94109',
+    },
+    {
+        id: 7, 
+        title: "test 7",
+        caption: 'caption7', 
+        address: '3 Bayside Village Pl, San Francisco, CA 94107',
     },
 ];
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,9 +1,9 @@
 /* This file contains the map assignment page. */
 
 import React from 'react';
-import { View, StyleSheet, Alert } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
 import MapView from 'react-native-maps';
-import { Marker } from 'react-native-maps';
+import { Marker, Callout } from 'react-native-maps';
 import * as Permissions from 'expo-permissions';
 import * as Location from 'expo-location';
 import { LocationAccuracy } from 'expo-location';
@@ -15,9 +15,11 @@ class MapScreen extends React.Component {
 
       this.state = {
         region:null,
+        coordinate:[],
       }
 
       this._getLocationAsync();
+      this._getCoordinatesAsync();
     }
 
     _getLocationAsync = async() => {
@@ -34,23 +36,57 @@ class MapScreen extends React.Component {
         longitudeDelta: 0.045
       }; 
 
-      this.setState({region: region});
+      this.setState({
+        region: region,
+      });
+    }
+
+    _getCoordinatesAsync = async() => {
+      let coordinates=[];
+
+      for(let i = 0; i < Assignments.length; i++){
+        const res = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${Assignments[i].address}&key=AIzaSyD80c8Ay-3S9r1lOg5rdgWoUg5fpzqupZ0`, {
+          method: 'GET',
+        });
+        let result = await res.json();
+        coordinates.push(result.results[0].geometry.location.lat);
+        coordinates.push(result.results[0].geometry.location.lng)
+      }
+      this.setState({
+        coordinate: coordinates
+      })
+      
     }
 
     render(){
       let markers = [];
+
       for(let i = 0; i < Assignments.length; i++){
         markers.push(
           <Marker
             key = {i}
             coordinate={{
-              latitude:Assignments[i].latitude,
-              longitude:Assignments[i].longitude,
+              latitude:this.state.coordinate[2 * i],
+              longitude:this.state.coordinate[2 * i + 1],
             }}
             icon={require('../assets/map_marker.png')}
             title={Assignments[i].title}
             description={Assignments[i].caption}
-          />
+          >
+            <Callout
+              tooltip
+              onPress={() => this.props.navigation.navigate('Camera')}
+            >
+              <View>
+                <View style={styles.bubble}>
+                  <Text style={styles.name}>{Assignments[i].id}. {Assignments[i].title}</Text>
+                  <Text>{Assignments[i].caption}</Text>
+                </View>
+                <View style={styles.arrowBorder} />
+                <View style={styles.arrow} />
+              </View>
+            </Callout>
+          </Marker>
         )
       }
       return (  
@@ -97,4 +133,42 @@ const styles = StyleSheet.create({
     right: 0,
     bottom: 0,
   },
+  // Callout bubble
+  bubble: {
+    flexDirection: 'column',
+    alignSelf: 'flex-start',
+    backgroundColor: '#fff',
+    borderRadius: 6,
+    borderColor: '#ccc',
+    borderWidth: 0.5,
+    padding: 15,
+    width: 150,
+  },
+  // Arrow below the bubble
+  arrow: {
+    backgroundColor: 'transparent',
+    borderColor: 'transparent',
+    borderTopColor: '#fff',
+    borderWidth: 16,
+    alignSelf: 'center',
+    marginTop: -32,
+  },
+  arrowBorder: {
+    backgroundColor: 'transparent',
+    borderColor: 'transparent',
+    borderTopColor: '#007a87',
+    borderWidth: 16,
+    alignSelf: 'center',
+    marginTop: -0.5,
+    // marginBottom: -15
+  },
+  // Character name
+  name: {
+    fontSize: 18,
+    marginBottom: 5,
+    fontWeight:"bold"
+  },
 });
+
+
+// google api key: AIzaSyD80c8Ay-3S9r1lOg5rdgWoUg5fpzqupZ0
